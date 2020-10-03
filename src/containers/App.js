294efebe35da0c4eefd9191d83090acc8934296f
Abdul Moiz Siraj <abdulmoiz68@gmail.com>
2020-10-03 23:02:49 +0500

    cleanup

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
-import CardList from "./CardList";
-import SearchBox from "./SearchBox";
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
 import "./App.css";
-import Scroll from "./scroll";
+import Scroll from "../components/scroll";
 
 class App extends Component {
   constructor() {
@@ -28,12 +28,11 @@ class App extends Component {
   }
 
   render() {
-    const filteredRobots = this.state.robots.filter((robot) => {
-      return robot.name
-        .toLowerCase()
-        .includes(this.state.searchfield.toLowerCase());
+    const { robots, searchfield } = this.state;
+    const filteredRobots = robots.filter((robot) => {
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     });
-    if (this.state.robots.length === 0) {
+    if (robots.length === 0) {
       return <h1 className="f1">loading</h1>;
     } else {
       return (
